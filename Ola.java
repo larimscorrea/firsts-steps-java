a779ea8ce4c2d78e33d3e5cd5c6809d10cc7c841
Larissa Corrêa <101229830+larimscorrea@users.noreply.github.com>
2022-09-13 23:53:12 -0300

    Add files via upload

---
+++ Ola.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+
+public class Ola { 
+	public static void main(String[] args) {
+		//TODO Auto.generated method stub 
+		System.out.println("Bom dia"); 
+		
+		Scanner leitor = new Scanner(System.in); 
+		String variavel; 
+		variavel = leitor.nextLine();
+		
+		System.out.println("Olá " + variavel);
+		
+	}
+}
